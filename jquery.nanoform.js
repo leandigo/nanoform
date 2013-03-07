3a1fdc431de8d20afd4d001e2c56c0352ea8266d
Michael Greenberg <michael@leandigo.com>
2013-03-07 12:59:26 +0000

    Added support fot input placeholder in IE

--- jquery.nanoform.js
+++ jquery.nanoform.js
@@ -27,22 +27,40 @@
 
 (function($) {
     $.fn.nanoform = function(options) {
-        var $input   = $(document.createElement('input')).addClass('nanoform-input').addClass('nanoform-anim').attr('placeholder', options.placeholder)
-        ,   $submit  = $(document.createElement('div')).addClass('nanoform-submit').html(options.submit_html)
-        ,   $overlay = $(document.createElement('div')).addClass('nanoform-overlay').addClass('nanoform-anim').html(options.overlay_html)
-        ,   $form    = this.append([$input, $submit, $overlay]).addClass('nanoform')
-        ,   ovl_w    = $overlay.width()
-        ,   input_w  = $input.width()
-        ,   submit_l = $submit.css('left')
-        ,   to_w     = options.to_w
-        ,   to_l     = options.to_l
-        ;
+        // Detect if the browser support a placeholder for an input field
+        var hasPlaceholder = 'placeholder' in document.createElement('input');
+        
+        if (hasPlaceholder) {
+            var $input   = $(document.createElement('input')).addClass('nanoform-input').addClass('nanoform-anim').attr('placeholder', options.placeholder)
+            ,   $submit  = $(document.createElement('div')).addClass('nanoform-submit').html(options.submit_html)
+            ,   $overlay = $(document.createElement('div')).addClass('nanoform-overlay').addClass('nanoform-anim').html(options.overlay_html)
+            ,   $form    = this.append([$input, $submit, $overlay]).addClass('nanoform')
+            ,   ovl_w    = $overlay.width()
+            ,   input_w  = $input.width()
+            ,   submit_l = $submit.css('left')
+            ,   to_w     = options.to_w
+            ,   to_l     = options.to_l
+            ;   
+        } else {
+            var $input   = $(document.createElement('input')).attr('id', 'nanoform-input').addClass('nanoform-input').addClass('nanoform-anim')
+            ,   $label   = $(document.createElement('label')).attr('for', 'nanoform-input').addClass('nanoform-placeholder').html(options.placeholder)
+            ,   $submit  = $(document.createElement('div')).addClass('nanoform-submit').html(options.submit_html)
+            ,   $overlay = $(document.createElement('div')).addClass('nanoform-overlay').addClass('nanoform-anim').html(options.overlay_html)
+            ,   $form    = this.append([$input, $label, $submit, $overlay]).addClass('nanoform')
+            ,   ovl_w    = $overlay.width()
+            ,   input_w  = $input.width()
+            ,   submit_l = $submit.css('left')
+            ,   to_w     = options.to_w
+            ,   to_l     = options.to_l
+            ;
+        }
         
         $submit.click(function() { options.fn($input.val()); } );
         $form.mouseenter(expandfield).mouseleave(collapsefield);
         $input.focusout(collapsefield);
         $input.keypress(function(event) {
-            if ( event.which == 13 ) {
+            $input.val().length > 0 ? $label.hide() : $label.fadeIn();
+            if (event.which == 13) {
                 options.fn($input.val());
             }
         });
@@ -59,6 +77,9 @@
                 $submit.animate({left: '+=' + to_l}, "fast");
                 $overlay.fadeOut({queue: false}, "fast").animate({width: '+=' + to_w, left: "+=-" + to_l}, "fast");
             }
+            if (!hasPlaceholder && !($input.val().length > 0)) {
+                $label.fadeIn();
+            }
         }
         
         function collapsefield() {
@@ -69,6 +90,9 @@
                     document.activeElement == $input[0] && $input.blur();
                     bindtouch();
                 });
+            }
+            if (!hasPlaceholder) {
+                $label.hide();
             }
         }
     };
